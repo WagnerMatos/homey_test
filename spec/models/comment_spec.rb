ae25b64c4d805cea3b40571cb661a8dae35bd84d
Wagner Matos <wagner.matos@mac.com>
2023-03-28 07:16:42 +0100

    Added body validation

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -7,4 +7,8 @@ RSpec.describe Comment, type: :model do
   describe "associations" do
     it { is_expected.to belong_to(:commentable) }
   end
+
+  describe "validations" do
+    it { should validate_presence_of(:body) }
+  end
 end
